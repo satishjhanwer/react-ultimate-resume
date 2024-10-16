73593ab93ab5b1e7f992d3b87b20e70a1dfb86bc
Satish Jhanwer <satish.jhanwer@gmail.com>
2024-10-16 21:23:38 +0530

    chore: latest changes merged

--- src/package/components/cards/cards_types/experiences/experiences_edit_dialog/experiences_edit_dialog.jsx
+++ src/package/components/cards/cards_types/experiences/experiences_edit_dialog/experiences_edit_dialog.jsx
@@ -61,6 +61,9 @@ const ExperiencesEditDialogComponent = ({ open, onClose, data, onEdit, validatio
                     defaultMessage="Edit your professional experiences?"
                 />
             }
+            classes={{
+                paper: 'w-[90vw] min-w-[300px] !max-w-[1280px]'
+            }}
         >
             {(helpers) => <ExperiencesEditFormWrapper helpers={helpers} />}
         </EditDialog>
@@ -211,27 +214,17 @@ const ExperienceItem = ({
                     </Typography>
                 </ListItem>
             </div>
-            <AnimatePresence>
-                {!folded && (
-                    <motion.div
-                        variants={EXPERIENCE_CONTENT_TRANSITION_PROPS}
-                        initial="initial"
-                        animate="animate"
-                        exit="exit"
-                        transition={spring}
-                    >
-                        <ContentFields
-                            fieldErrors={fieldErrors}
-                            id={id}
-                            formatMessage={formatMessage}
-                            experience={experience}
-                            onChange={onChange}
-                            classes={classes}
-                            index={index}
-                        />
-                    </motion.div>
-                )}
-            </AnimatePresence>
+            {!folded && (
+                <ContentFields
+                    fieldErrors={fieldErrors}
+                    id={id}
+                    formatMessage={formatMessage}
+                    experience={experience}
+                    onChange={onChange}
+                    classes={classes}
+                    index={index}
+                />
+            )}
         </div>
     );
 };
@@ -261,10 +254,10 @@ const ContentFields = ({ fieldErrors, id, formatMessage, experience, onChange, c
     ]);
 
     return (
-        <div className={classes.content}>
-            <div className={classes.line} />
+        <div className="flex items-stretch">
+            <div className={'w-[1px] bg-dark-50 mx-2'} />
             <div className={classes.fields}>
-                <div className={classes.fieldRow}>
+                <div className={'flex flex-wrap mb-1'}>
                     <div className={classes.fieldContainer}>
                         <Typography component="p" color="dark" variant="label">
                             {formatMessage(translations.companyName)}
@@ -301,8 +294,6 @@ const ContentFields = ({ fieldErrors, id, formatMessage, experience, onChange, c
                             </Typography>
                         )}
                     </div>
-                </div>
-                <div className={classes.fieldRow}>
                     <div className={classes.fieldContainer}>
                         <Typography component="p" color="dark" variant="label">
                             {formatMessage(translations.jobPlace)}
@@ -323,34 +314,35 @@ const ContentFields = ({ fieldErrors, id, formatMessage, experience, onChange, c
                         )}
                     </div>
                 </div>
-                <div className={cn(classes.fieldRow, classes.yearMonthRow)}>
-                    <div className={classes.yearMonthWrapper}>
+                <div className={'flex flex-wrap mb-1'}>
+                    {/*<div className={classes.yearMonthWrapper}>*/}
+                    <div className={classes.fieldContainer}>
+                        <YearMonth
+                            textfieldProps={{ fullWidth: true }}
+                            variant="flat"
+                            value={experience.startDate}
+                            onChange={handleStartDate}
+                            title={translations.startDate}
+                            error={fieldErrors?.startDate}
+                        />
+                    </div>
+                    {!stillEmployed && (
                         <div className={classes.fieldContainer}>
                             <YearMonth
-                                textfieldProps={{ fullWidth: true }}
                                 variant="flat"
-                                value={experience.startDate}
-                                onChange={handleStartDate}
-                                title={translations.startDate}
-                                error={fieldErrors?.startDate}
+                                value={experience.endDate}
+                                onChange={handleEndDate}
+                                title={translations.endDate}
+                                error={fieldErrors?.endDate}
                             />
                         </div>
-                        {!stillEmployed && (
-                            <div className={classes.fieldContainer}>
-                                <YearMonth
-                                    variant="flat"
-                                    value={experience.endDate}
-                                    onChange={handleEndDate}
-                                    title={translations.endDate}
-                                    error={fieldErrors?.endDate}
-                                />
-                            </div>
-                        )}
-                    </div>
-                    {stillEmployed && stillEmployedComponent}
+                    )}
+                    {stillEmployedComponent}
+
+                    {/*</div>*/}
                 </div>
-                {!stillEmployed && stillEmployedComponent}
-                <div className={classes.fieldRow}>
+
+                <div className={'flex flex-wrap'}>
                     <div className={cn(classes.fieldContainer, classes.fullWidthFieldContainer)}>
                         <Typography component="p" color="dark" variant="label">
                             {formatMessage(translations.descriptionTitle)}
@@ -483,7 +475,7 @@ const ExperiencesEditForm = ({ data, errors, onAdd, onMove, onFieldChange, onDel
     const globalError = typeof errors === 'string' && errors;
 
     return (
-        <div className={classes.container}>
+        <>
             <SortableExperiences
                 onSortEnd={onMove}
                 items={data}
@@ -502,7 +494,7 @@ const ExperiencesEditForm = ({ data, errors, onAdd, onMove, onFieldChange, onDel
                     {errors}
                 </Typography>
             )}
-        </div>
+        </>
     );
 };
 
--- src/package/components/commons/profile_card/profile_card_incomplete_popper/profile_card_incomplete_popper.jsx
+++ src/package/components/commons/profile_card/profile_card_incomplete_popper/profile_card_incomplete_popper.jsx
@@ -1,19 +1,12 @@
 import React, { memo, useEffect, useState } from 'react';
-
-import makeStyles from '@mui/styles/makeStyles';
 import { FormattedMessage } from 'react-intl';
 
 import { PopperCard, Typography } from '@welovedevs/ui';
 
 import { ReactComponent as WarnIcon } from '../../../../assets/icons/warn.svg';
-
-import { styles } from './profile_card_incomplete_popper_styles';
 import { useMode } from '../../../hooks/use_mode';
 
-const useStyles = makeStyles(styles);
-
 const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) => {
-    const classes = useStyles();
     const [hasBeenMounted, setHasBeenMouneted] = useState(false);
     const [mode] = useMode();
 
@@ -26,16 +19,22 @@ const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) 
     return (
         <PopperCard
             classes={{
-                container: classes.container,
-                arrowContainer: classes.arrowContainer
+                container: 'bg-danger-400 ',
+                arrowContainer: 'text-danger-400 '
             }}
             open={open}
             onClose={onClose}
             anchorElement={anchorElement}
             popperProps={{
-                placement: 'top',
+                placement: 'top-start',
                 disablePortal: true,
                 modifiers: [
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, -36]
+                        }
+                    },
                     {
                         name: 'preventOverflow'
                     },
@@ -43,13 +42,14 @@ const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) 
                         name: 'hide'
                     },
                     {
-                        name: 'flip'
+                        name: 'flip',
+                        enabled: false
                     }
                 ]
             }}
         >
-            <WarnIcon className={classes.icon} />
-            <Typography color="light">
+            <Typography color="light" className="flex items-center">
+                <WarnIcon className="mr-1 w-3 h-3 fill-current" />
                 <FormattedMessage
                     id="ProfileCardIncompletePopper.label.value"
                     defaultMessage="This card is missing data"
